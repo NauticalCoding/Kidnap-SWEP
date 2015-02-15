6659de9e4abe235c2886afa82eeb4489dd704f9e
NauticalCoding <lordofhens@gmail.com>
2015-02-15 18:02:30 -0500

    Ragdoll movement ( 2 )

--- lua/weapons/weapon_kidnap/shared.lua
+++ lua/weapons/weapon_kidnap/shared.lua
@@ -90,7 +90,7 @@ local function altPickup(ply,ent)
 				hook.Remove("Think",ply.pickupObjectTable[2])	
 			end
 			
-			local vel = difference * ( difference:Length() * 2 )
+			local vel = difference * ( difference:Length() * 10 )
 			
 			vel.x = math.Clamp(vel.x,-clamp,clamp)
 			vel.y = math.Clamp(vel.y,-clamp,clamp)
